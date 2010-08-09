cbc03211afecb93c346aa027ca74968ed2b1a817
Jason Clifford <jason@ukfsn.org>
2010-08-09 18:34:59 +0100

    everything except order is now complete

--- lib/Net/Trustico.pm
+++ lib/Net/Trustico.pm
@@ -9,6 +9,42 @@ use Carp qw/croak/;
 use base 'Class::Accessor';
 __PACKAGE__->mk_accessors(qw/username password/);
 
+my %products = (
+    'freessl30' => {
+        name => 'FreeSSL 30 Day Trial',
+        periods => [ qw/1/ ],
+        vetting => 'DOM',
+        process => '1',
+        reissuance => 0,
+        canrenew => 0
+    },
+    'rapidssl' => {
+        name => 'RapidSSL',
+        periods => [ qw/12 24 36 48 60/ ],
+        vetting => 'DOM',
+        process => '1',
+        reissuance => 1,
+        canrenew => 1,
+    },
+    rapidsslwildcard => {
+        name => 'RapidSSL Wildcard',
+        periods => [ qw/12 24 36 48 60/ ],
+        vetting => 'DOM',
+        process => '1',
+        reissuance => 1,
+        canrenew => 1,
+    },
+    geotrust30 => {
+        name => 'GeoTrust SSL 30 Day Trial',
+        periods => [ qw/1/ ],
+        vetting => 'DOM',
+        process => '1',
+        reissuance => 0,
+        canrenew => 0
+    },
+    # XXX Need to add remaining products
+    );
+
 =head1 NAME
 
 Net::Trustico - Perl extension for ordering SSL certificates from Trustico
@@ -20,9 +56,26 @@ via their API.
   
   my $t = Net::Trustico->new( username => $user, password => $pass );
 
-  my $products = $t->products();
+  die unless $t->hello( $testString );
 
-  $t->hello( $testString );
+  my $a = {
+    title => 'Ms',
+    firstname => 'Eliza',
+    lastname => 'Xample',
+    organisation => 'E.Xample',
+    role => 'WebSite Owner',
+    email => 'e.xample@example.com',
+    phonecc => '44',
+    phoneac => '020',
+    phonen => '9460234',
+    address1 => '1 High Street',
+    city => 'MyTown',
+    state => 'London',
+    postcode => 'SW1 4AA',
+    country => 'GB'
+  };
+
+  my $t = $a;
 
   my %result = $t->order( product => $product,
                           csr => $csr,
@@ -31,7 +84,7 @@ via their API.
                           insurance => 0,
                           servercount => 1,
                           admin => $a,
-                          tech => $t,
+                          techusereseller => 1,
                           novalidation => 0
                           );
 
@@ -44,6 +97,14 @@ Perl module for ordering SSL certificates from Trustico.
 =head1 METHODS
 
 =head2 new
+
+Initiates the module. 
+
+Parameters:
+
+username    - your Trustico reseller account username
+
+password    - the password for your reseller account.
 
 =head2 hello
 
@@ -60,9 +121,53 @@ no parameters are passed a standard string is used to test the connection.
 Submits an order to the Trustico API and returns a hash reference 
 confirming order details on success or undef.
 
-See the Trustico documentation for relevent fields but note that the
-Admin and Tech contacts should be passed as hash references rather than
-individual fields.
+Parameters:
+
+product     - the product code for the relevant product as provided by
+              the products() method.
+
+csr         - the CSR for the certificate
+
+period      - period for the certificate in months. Valid options are
+              detailed in the details provided by the products() method.
+
+approver    - Approver email address. Must be one of admin, administrator
+              hostmaster, root, webmaster or postmaster prepended to the
+              domain supplied in the request
+
+insurance   - Re-issue insurance required - 1 or 0
+
+servercount - Number of server licenses requires. 
+
+novalidation- If set to 1 the CSR can be blank to be provded later via the
+              Trustico reseller management interface.
+
+special     - Special instructions to issuer. Up to 255 characters
+
+admin       - Admin contact details hash ref
+
+tech        - Tech contact details hash ref
+
+org         - Organisation details hash ref (required for products with 
+              ORG vetting type only - will be ignored if not required)
+
+Tech contact hash ref must contain either the following fields:
+
+    title, firstname, lastname, organisation, email, phonecc, phoneac, 
+    phonen, address1, city, state, postcode, country.
+
+The tech contact may also contain an optional address2 field
+
+Alternatively the tech contact may be omitted if the techusereseller
+field is set to 1 in which case the default details provided via the 
+Trustico reseller control panel will be used.
+
+Admin contact hash must contain all of the fields required for the Tech
+contact plus a role field.
+
+The admin contact may also contain the following optional fields:
+
+    taxid, memdate (ISO format)
 
 =head2 status
 
@@ -72,8 +177,8 @@ The returned details are in a hash reference.
 
 =head2 products
 
-Returns a hash reference containing a list of product codes and the 
-product name for each code.
+Returns a hash containing a list of product codes and the details for each
+product.
 
 =head1 SEE ALSO
 
@@ -101,12 +206,22 @@ sub hello {
     $string = "Net::Trustico test string" unless $string;
 
     my $res = $self->_req('Hello', { 'TextToEcho' => $string } );
-    return undef unless $res->{SuccessCode} == 1;
     return undef unless $res->{TextToEcho} eq $string;
     return 1;
 }
 
 sub order {
+    my ($self, %args) = @_;
+
+    my $args = { };
+    my $command = 'ProcessType1';
+    if ( $products{$args{product}->{process}} != 1 ) {
+        $command = 'ProcessType2';
+    }
+
+    # XXX process %args into correct fields for $command
+    
+    my $res = $self->_req($command, $args);
 }
 
 sub status {
@@ -121,16 +236,10 @@ sub status {
         $args->{IssuerOrderID} = $args{issuerid};
     }
 
-    my $res = $self->_req('GetStatus', $args);
-    return undef unless $res->{SuccessCode} == 1;
-    # XXX Process returned data
+    $self->_req('GetStatus', $args);
 }
 
-sub _process_type_1 {
-}
-
-sub _process_type_2 {
-}
+sub products { return %products; }
 
 sub _req {
     my ($self, $command, $args) = @_;
@@ -159,6 +268,9 @@ sub _req {
         $_ =~ /(.*?)\|(.*?)\|/;
         $rv{$1} = $2;
     }
+
+    croak $rv{Error} if $rv{SuccessCode} == 0;
+    delete $rv{$_} for (qw/Error SuccessCode/);
 
     return \%rv;
 }
